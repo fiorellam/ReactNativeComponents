6927af3dc55ef20ca729bd0ad406c03414663767
unknown <fiorellaroma@hotmail.com>
2020-12-21 23:57:01 -0800

    Agregando diseno de login

--- app/views/login.js
+++ app/views/login.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import { View, Text, Button} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { View, Text, ImageBackground, StyleSheet} from 'react-native';
+import { Input, Button } from 'react-native-elements';
 
 
 const Login = ({navigation}) => {
   return (
-    <>
-    <Text>Login</Text>
-    <Button onPress={() => navigation.navigate('CreateUser')} title="CrearUsuario">Crear Usuario</Button>
-
-    </>
+    <ImageBackground source={require("../assets/Bg.png")} style={styles.image}>
+      <View style={styles.inputs}>
+        <Text style={styles.title}>Login</Text>
+        <Input
+          placeholder='Email'
+          leftIcon={{ type: 'font-awesome', name: 'envelope-o' }}
+        />
+        <Button 
+          onPress={() => navigation.navigate('CreateUser')} 
+          title="Crear Usuario"
+          buttonStyle={{ backgroundColor: "#9CCB5B" }}
+        />
+      </View>
+    </ImageBackground>
   );
 };
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    paddingBottom: 30
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+  inputs: {
+    flex:1,
+    alignContent: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 30
+  },
+
+})
+
 
 export default Login;
